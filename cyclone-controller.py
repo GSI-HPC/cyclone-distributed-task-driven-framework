a4669121589224f51e28cabd291e9637a069d366
Gabriele Iannetti <g.iannetti@gsi.de>
2021-01-26 12:27:38 +0100

    complete signal interrupt activation for controller

--- cyclone-controller.py
+++ cyclone-controller.py
@@ -231,10 +231,11 @@ def main():
                 logging.debug("Version: %s", VERSION)
 
                 signal.signal(signal.SIGINT, signal.SIG_IGN)
-
                 signal.signal(signal.SIGHUP, signal_handler)
                 signal.signal(signal.SIGTERM, signal_handler)
 
+                signal.siginterrupt(signal.SIGHUP, True)
+                signal.siginterrupt(signal.SIGINT, True)
                 signal.siginterrupt(signal.SIGTERM, True)
 
                 comm_handler.connect()
