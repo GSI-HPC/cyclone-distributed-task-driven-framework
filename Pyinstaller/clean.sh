68561bc8c3278bf4aa31d8057524f1fea676795c
Gabriele Iannetti <g.iannetti@gsi.de>
2023-11-16 11:56:38 +0100

    update licence

--- Pyinstaller/clean.sh
+++ Pyinstaller/clean.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# -*- coding: utf-8 -*-
+#
+# © Copyright 2023 GSI Helmholtzzentrum für Schwerionenforschung
+#
+# This software is distributed under
+# the terms of the GNU General Public Licence version 3 (GPL Version 3),
+# copied verbatim in the file "LICENCE".
 
 TARGET_DIR=pybuild
 
